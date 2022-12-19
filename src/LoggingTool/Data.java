d250bd299d30efd6806bac5d09fae98434985c79
Martin Cruces <martin76e@yahoo.com>
2022-12-19 13:13:06 +0100

    added if with enums instead of switch.

--- src/LoggingTool/Data.java
+++ src/LoggingTool/Data.java
@@ -2,6 +2,11 @@ package LoggingTool;
 
 public enum Data {
     // enums med states om du är receptionist eller djurhanterare.
-    RECEPTIONIST,
-    HANDLER
+
+    RECEPTIONIST ("1"),
+    HANDLER("2");
+
+    final String data;
+
+    Data(String s) { data = s;}
 }
